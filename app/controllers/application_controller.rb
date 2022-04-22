16794d31c8eb1ccda403811555d8e8229a271fd7
Elizabeth Clawson <emclawson1@gmail.com>
2022-04-22 15:11:17 -0500

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  Rails.application.routes.draw do
+    resources :plants, only: [:index, :show, :create]
+  end
+  
 end
